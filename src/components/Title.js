ff7ba8195188cebc84c5793e6ea4f62fe84b6383
David Adeola <adeoladavid15@gmail.com>
2018-11-07 19:05:36 +0300

    Created the weather, form and titlecomponents' basic structure

---
+++ src/components/Title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Title(){
+  return(
+    <div>
+      <h1>Weather App</h1>
+      <p>Find out temperature conditions and more...</p>
+    </div>
+  );
+}
+
+export default Title;
