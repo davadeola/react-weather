b7503b11c7f8e082b391d974f15f7c03e38eb14e
David Adeola <adeoladavid15@gmail.com>
2018-11-07 19:22:12 +0300

    Created the getWeather() which utilises the fetch api

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import Title from './components/Title.js';
 import Weather from './components/Weather.js'
 import Form from './components/Form.js'
 
+const API_KEY = 'd1d487c71ce055800753277f31bce34f';
+
 class App extends React.Component{
+
+  getWeather= async ()=>{
+      const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&appid=${API_KEY}$units=metric`);
+      const data =  await api_call.json();//converts the data received to JSON format
+  }
+
   render(){
     return(
       <div>
